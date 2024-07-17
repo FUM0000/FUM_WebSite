1363d059ab5a14625521b757e2677929ed7a3270
FUM <53284113+fuhito516@users.noreply.github.com>
2024-07-17 19:52:57 +0900

    the index design

--- Asset/Javascript/common.js
+++ Asset/Javascript/common.js
@@ -36,3 +36,50 @@ function Change_BGM_Name(_name) {
 function RandomNumber(_min, _max) {
     return Math.random() * (_max - _min + 1) + _min;
 }
+
+
+// Index
+function Resize() {
+
+
+    // Variables
+    const screen_width = $("#main-container").width();
+    const screen_height = $("#main-container").height();
+    const screen_narrower = Math.min(screen_width, screen_height);
+    const width_center = screen_width * 0.5;
+    const height_center = screen_height * 0.35;
+    let radian = 0;
+    let radius = 0;
+    let item_width = 0;
+    let item_height = 0;
+    let target_x = 0;
+    let target_y = 0;
+    let item_left = 0;
+    let item_top = 0;
+    let bottom = 0;
+
+
+    for(let i = 1; i < 16; i++) {
+        // Calculate
+        item_width = $(".item-" + i).width();
+        item_height = $(".item-" + i).height();
+        target_x = width_center + (radius * Math.cos(radian));
+        target_y = height_center + (radius * Math.sin(radian));
+        item_left = target_x - item_width / 2;
+        item_top = target_y - item_height / 2;
+        bottom = item_top > bottom ? item_top : bottom;
+
+        // Apply
+        $(".item-" + i).css("left", item_left);
+        $(".item-" + i).css("top", item_top);
+
+        // Prepare for the next
+        radian += Math.PI / 5;
+        radius = Math.max(0, (Math.min(screen_narrower * 0.09, 45) - (i - 1)) * radian);
+    }
+    
+    // Welcome
+    const welcome_top = bottom + (screen_height - bottom) * 0.5;
+    $(".welcome_1").css("left", width_center - $(".welcome_1").width * 0.5);
+    $(".welcome_1").css("top", welcome_top);
+}
