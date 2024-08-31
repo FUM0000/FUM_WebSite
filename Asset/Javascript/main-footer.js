54c2bcd051dd5c1fb5375101f9e25df823568a06
FUM <53284113+fuhito516@users.noreply.github.com>
2024-08-31 23:17:28 +0900

    ブログ修正と最下部まで飛ぶ機能実装。飛ぶ機能は滑らかにする必要あり

--- Asset/Javascript/main-footer.js
+++ Asset/Javascript/main-footer.js
@@ -1,6 +1,9 @@
 Vue.component('main-footer', {
     template: `
     <v-footer color="#CFD8DC80" class="font-weight-black" app>
+        <v-btn @click="scrollToBottom" text x-small>
+            <v-icon color="blue">mdi-chevron-down</v-icon>
+        </v-btn>
         <slot></slot>
         <v-spacer />
         <v-btn @click="$vuetify.goTo(0)" text x-small>
@@ -8,4 +11,9 @@ Vue.component('main-footer', {
         </v-btn>
     </v-footer>
     `,
+    methods: {
+        scrollToBottom() {
+            window.scrollTo(0, document.body.scrollHeight);
+        }
+    }
 })
