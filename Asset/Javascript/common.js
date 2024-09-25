b6fcac65711c769e4a04a11b2069e2b33d2bee71
FUM <53284113+fuhito516@users.noreply.github.com>
2024-09-26 01:56:32 +0900

    organize

--- Asset/Javascript/common.js
+++ Asset/Javascript/common.js
@@ -83,3 +83,37 @@ function Resize() {
     $(".welcome_1").css("left", width_center - $(".welcome_1").width * 0.5);
     $(".welcome_1").css("top", welcome_top);
 }
+
+Vue.component('main-footer', {
+    template: `
+    <v-footer color="#CFD8DC80" class="font-weight-black" app>
+        <v-btn @click="scrollToBottom" text x-small>
+            <v-icon color="blue">mdi-chevron-down</v-icon>
+        </v-btn>
+        <v-spacer />
+        <span class="copyright">\u00A9 2024 FUM / \u00A9 2024 FUM_WebSite</span>
+        <v-spacer />
+        <v-btn @click="scrollToTop" text x-small>
+            <v-icon color="red">mdi-chevron-up</v-icon>
+        </v-btn>
+    </v-footer>
+    `,
+    methods: {
+        scrollToBottom() {
+            this.$vuetify.goTo(document.body.scrollHeight);
+        },
+        scrollToTop() {
+            this.$vuetify.goTo(0);
+        }
+    }
+})
+
+Vue.component('main-footer-simple', {
+    template: `
+    <v-footer color="#CFD8DC80" class="font-weight-black" app>
+        <v-spacer />
+        <span class="copyright">\u00A9 2024 FUM / \u00A9 2024 FUM_WebSite</span>
+        <v-spacer />
+    </v-footer>
+    `,
+})
