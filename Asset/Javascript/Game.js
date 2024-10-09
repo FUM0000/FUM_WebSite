f5fc2651fa389923b7423d4f18d377fbab0386cd
FUM <53284113+fuhito516@users.noreply.github.com>
2024-10-10 04:34:51 +0900

    Update Game

--- Asset/Javascript/Game.js
+++ Asset/Javascript/Game.js
@@ -1,13 +1,8 @@
 
-// Funcion
-//// Random
 function RandomNumber_Between(_min, _max) { return Math.random() * (_max - _min + 1) + _min; }
-
-// window.Environment_baseclass        = Environment_baseclass;
-// window.Camera_baseclass             = Camera_baseclass;
-// window.Light_Ambient_baseclass      = Light_Ambient_baseclass;
-// window.Light_Directional_baseclass  = Light_Directional_baseclass;
-// window.Audio_baseclass              = Audio_baseclass;
-// window.Timer_baseclass              = Timer_baseclass;
-// window.TextPlane_baseclass          = TextPlane_baseclass;
-// window.Manager_baseclass            = Manager_baseclass;
+function Degrees_to_Radians(_degrees) { return _degrees * (Math.PI / 180); }
+function Alpha_Change(_color, _alpha) {
+    if (_color.length === 7) { return _color + _alpha; }
+    if (_color.length === 9) { return _color.slice(0, 7) + _alpha; }
+    return null;
+}
