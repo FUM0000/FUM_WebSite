ca70f1f4e3c73063a06ac8db5eb415238ba48162
FUM <53284113+fuhito516@users.noreply.github.com>
2024-10-11 21:43:05 +0900

    up

--- Asset/Javascript/Game.js
+++ Asset/Javascript/Game.js
@@ -1,4 +1,6 @@
 
+import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
+
 function Mobile_or_Desktop() { return /Mobi|Android/i.test(navigator.userAgent); }
 function RandomNumber_Between(_min, _max) { return Math.random() * (_max - _min + 1) + _min; }
 function Degrees_to_Radians(_degrees) { return _degrees * (Math.PI / 180); }
@@ -7,3 +9,10 @@ function Alpha_Change(_color, _alpha) {
     if (_color.length === 9) { return _color.slice(0, 7) + _alpha; }
     return null;
 }
+
+class FC_GameObject {
+    Initialize  () {}
+    Update      () {}
+}
+
+export { FC_GameObject }
