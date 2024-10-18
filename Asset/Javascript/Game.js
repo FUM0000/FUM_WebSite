7bb428da2578b45dbe8e6c24ddb233ec96bb5133
FUM <53284113+fuhito516@users.noreply.github.com>
2024-10-18 16:58:45 +0900

    debug

--- Asset/Javascript/Game.js
+++ Asset/Javascript/Game.js
@@ -3,6 +3,9 @@ import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
 
 function Mobile_or_Desktop() { return /Mobi|Android/i.test(navigator.userAgent); }
 function RandomNumber_Between(_min, _max) { return Math.random() * (_max - _min + 1) + _min; }
+function Shousuu_Kirisute(_number, _keta) {
+    return Math.floor(_number * Math.pow(10, _keta)) / Math.pow(10, _keta);
+}
 function Degrees_to_Radians(_degrees) { return _degrees * (Math.PI / 180); }
 function Alpha_Change(_color, _alpha) {
     if (_color.length === 7) { return _color + _alpha; }
@@ -615,6 +618,7 @@ class FC_Manager {
 export {
     Mobile_or_Desktop,
     RandomNumber_Between,
+    Shousuu_Kirisute,
     Degrees_to_Radians,
     Alpha_Change,
     FC_JoystickController,
