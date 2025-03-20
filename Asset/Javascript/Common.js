5bca76853e80955b570c7035ead6ab7b7fcee4fe
FUM <53284113+fuhito516@users.noreply.github.com>
2025-03-20 23:10:48 +0900

    rename and organize

--- Asset/Javascript/Common.js
+++ Asset/Javascript/Common.js
@@ -87,44 +87,6 @@ Vue.component('main-navigation', {
             <!-- ▲ Home ▲ ------------------------------------------------------------------------------------->
 
 
-            <!-- ▼ Function ▼ --------------------------------------------------------------------------------->
-            <v-list-group :value="false" prepend-icon="mdi-hammer-wrench">
-                <template v-slot:activator>
-                    <v-list-item-title>Function</v-list-item-title>
-                </template>
-
-                <v-list-item href="./Function_BezierCurve.html">
-                    <v-list-item-icon />
-                    <v-list-item-title>Bezier Curve</v-list-item-title>
-                </v-list-item>
-                <v-list-item href="./Function_ColorDesign.html">
-                    <v-list-item-icon />
-                    <v-list-item-title>Color Design</v-list-item-title>
-                </v-list-item>
-                <v-list-item href="./Function_Converter.html">
-                    <v-list-item-icon />
-                    <v-list-item-title>Converter</v-list-item-title>
-                </v-list-item>
-                <v-list-item href="./Function_FontPreview.html">
-                    <v-list-item-icon />
-                    <v-list-item-title>Font Preview</v-list-item-title>
-                </v-list-item>
-                <v-list-item href="./Function_ImageEditor.html">
-                    <v-list-item-icon />
-                    <v-list-item-title>Image Editor</v-list-item-title>
-                </v-list-item>
-                <v-list-item href="./Function_JapaneseCalendar.html">
-                    <v-list-item-icon />
-                    <v-list-item-title>Japanese Calendar</v-list-item-title>
-                </v-list-item>
-                <v-list-item href="./Function_QRCode.html">
-                    <v-list-item-icon />
-                    <v-list-item-title>QR Code</v-list-item-title>
-                </v-list-item>
-            </v-list-group>
-            <!-- ▲ Function ▲ --------------------------------------------------------------------------------->
-
-
             <!-- ▼ Japanese ▼ --------------------------------------------------------------------------------->
             <v-list-group :value="false" prepend-icon="mdi-translate-variant">
                 <template v-slot:activator>
@@ -521,18 +483,42 @@ Vue.component('main-navigation', {
             <!-- ▲ Japanese ▲ --------------------------------------------------------------------------------->
 
 
-            <!-- ▼ Programming ▼ ------------------------------------------------------------------------------>
-            <v-list-group :value="false" prepend-icon="mdi-laptop">
+            <!-- ▼ Service ▼ ---------------------------------------------------------------------------------->
+            <v-list-group :value="false" prepend-icon="mdi-hammer-wrench">
                 <template v-slot:activator>
-                    <v-list-item-title>Programming</v-list-item-title>
+                    <v-list-item-title>Service</v-list-item-title>
                 </template>
 
-                <v-list-item href="./Programming_Introduction.html">
+                <v-list-item href="./Service_BezierCurve.html">
                     <v-list-item-icon />
-                    <v-list-item-title>Introduction</v-list-item-title>
+                    <v-list-item-title>Bezier Curve</v-list-item-title>
+                </v-list-item>
+                <v-list-item href="./Service_ColorDesign.html">
+                    <v-list-item-icon />
+                    <v-list-item-title>Color Design</v-list-item-title>
+                </v-list-item>
+                <v-list-item href="./Service_Converter.html">
+                    <v-list-item-icon />
+                    <v-list-item-title>Converter</v-list-item-title>
+                </v-list-item>
+                <v-list-item href="./Service_FontPreview.html">
+                    <v-list-item-icon />
+                    <v-list-item-title>Font Preview</v-list-item-title>
+                </v-list-item>
+                <v-list-item href="./Service_ImageEditor.html">
+                    <v-list-item-icon />
+                    <v-list-item-title>Image Editor</v-list-item-title>
+                </v-list-item>
+                <v-list-item href="./Service_JapaneseCalendar.html">
+                    <v-list-item-icon />
+                    <v-list-item-title>Japanese Calendar</v-list-item-title>
+                </v-list-item>
+                <v-list-item href="./Service_QRCode.html">
+                    <v-list-item-icon />
+                    <v-list-item-title>QR Code</v-list-item-title>
                 </v-list-item>
             </v-list-group>
-            <!-- ▲ Programming ▲ ------------------------------------------------------------------------------>
+            <!-- ▲ Service ▲ ---------------------------------------------------------------------------------->
 
 
             <!-- ▼ Cooking ▼ ---------------------------------------------------------------------------------->
@@ -608,6 +594,48 @@ Vue.component('main-navigation', {
             </v-list-group>
             <!-- ▲ Music ▲ ------------------------------------------------------------------------------------>
             
+
+            <!-- ▼ Game ▼ ------------------------------------------------------------------------------------->
+            <v-list-group :value="false" prepend-icon="mdi-nintendo-game-boy">
+                <template v-slot:activator>
+                    <v-list-item-title>Game</v-list-item-title>
+                </template>
+                
+                <v-list-item href="./Game_RandomQuestion.html">
+                    <v-list-item-icon />
+                    <v-list-item-title>Random Question</v-list-item-title>
+
+                    <v-list-item-icon>
+                        <v-icon>mdi-comment-question-outline</v-icon>
+                    </v-list-item-icon>
+                </v-list-item>
+                <v-list-item href="./Game_ReflexSpeed.html">
+                    <v-list-item-icon />
+                    <v-list-item-title>Reflex Speed</v-list-item-title>
+                        
+                    <v-list-item-icon>
+                        <v-icon>mdi-speedometer-slow</v-icon>
+                    </v-list-item-icon>
+                </v-list-item>
+                <v-list-item href="./Game_Roulette.html">
+                    <v-list-item-icon />
+                    <v-list-item-title>Roulette</v-list-item-title>
+                    
+                    <v-list-item-icon>
+                        <v-icon>mdi-pizza</v-icon>
+                    </v-list-item-icon>
+                </v-list-item>
+                <v-list-item href="./Game_KittyHopper.html">
+                    <v-list-item-icon />
+                    <v-list-item-title>Kitty Hopper</v-list-item-title>
+                        
+                    <v-list-item-icon>
+                        <v-icon>mdi-cat</v-icon>
+                    </v-list-item-icon>
+                </v-list-item>
+            </v-list-group>
+            <!-- ▲ Game ▲ ------------------------------------------------------------------------------------->
+
 
             <!-- ▼ Plant ▼ ------------------------------------------------------------------------------------>
             <v-list-group :value="false" prepend-icon="mdi-sprout">
@@ -711,48 +739,6 @@ Vue.component('main-navigation', {
             <!-- ▲ Plant ▲ ------------------------------------------------------------------------------------>
 
 
-            <!-- ▼ Game ▼ ------------------------------------------------------------------------------------->
-            <v-list-group :value="false" prepend-icon="mdi-nintendo-game-boy">
-                <template v-slot:activator>
-                    <v-list-item-title>Game</v-list-item-title>
-                </template>
-                
-                <v-list-item href="./Game_RandomQuestion.html">
-                    <v-list-item-icon />
-                    <v-list-item-title>Random Question</v-list-item-title>
-
-                    <v-list-item-icon>
-                        <v-icon>mdi-comment-question-outline</v-icon>
-                    </v-list-item-icon>
-                </v-list-item>
-                <v-list-item href="./Game_ReflexSpeed.html">
-                    <v-list-item-icon />
-                    <v-list-item-title>Reflex Speed</v-list-item-title>
-                        
-                    <v-list-item-icon>
-                        <v-icon>mdi-speedometer-slow</v-icon>
-                    </v-list-item-icon>
-                </v-list-item>
-                <v-list-item href="./Game_Roulette.html">
-                    <v-list-item-icon />
-                    <v-list-item-title>Roulette</v-list-item-title>
-                    
-                    <v-list-item-icon>
-                        <v-icon>mdi-pizza</v-icon>
-                    </v-list-item-icon>
-                </v-list-item>
-                <v-list-item href="./Game_KittyHopper.html">
-                    <v-list-item-icon />
-                    <v-list-item-title>Kitty Hopper</v-list-item-title>
-                        
-                    <v-list-item-icon>
-                        <v-icon>mdi-cat</v-icon>
-                    </v-list-item-icon>
-                </v-list-item>
-            </v-list-group>
-            <!-- ▲ Game ▲ ------------------------------------------------------------------------------------->
-
-
             <!-- ▼ Japan ▼ ------------------------------------------------------------------------------------>
             <v-list-group :value="false" prepend-icon="mdi-home-city-outline">
                 <template v-slot:activator>
@@ -828,6 +814,20 @@ Vue.component('main-navigation', {
                 </v-list-item>
             </v-list-group>
             <!-- ▲ Japan ▲ ------------------------------------------------------------------------------------>
+
+
+            <!-- ▼ Programming ▼ ------------------------------------------------------------------------------>
+            <v-list-group :value="false" prepend-icon="mdi-laptop">
+                <template v-slot:activator>
+                    <v-list-item-title>Programming</v-list-item-title>
+                </template>
+
+                <v-list-item href="./Programming_Introduction.html">
+                    <v-list-item-icon />
+                    <v-list-item-title>Introduction</v-list-item-title>
+                </v-list-item>
+            </v-list-group>
+            <!-- ▲ Programming ▲ ------------------------------------------------------------------------------>
 
 
             <!-- ▼ About me ▼ --------------------------------------------------------------------------------->
